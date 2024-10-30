eaa01636eb25c54288b0fbc3c5c83a02c4af02f7
Glenn Keates <glennrkeates@gmail.com>
2024-10-30 19:14:37 +0000

    Add pin state management to MockGpioDriver
    * **Pin State Storage**
      - Add a private dictionary `_pinStates` to store the state of each pin.
    
    * **Write Method Update**
      - Update the `Write` method to store the value being written to the pin in `_pinStates`.
    
    * **Read Method Update**
      - Update the `Read` method to return the stored value from `_pinStates` if it exists, otherwise return `PinValue.Low`.

--- RemoteRelay/MockGpioDriver.cs
+++ RemoteRelay/MockGpioDriver.cs
@@ -2,11 +2,14 @@ using System;
 using System.Device.Gpio;
 using System.Device.Gpio.Drivers;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace RemoteRelay
 {
     public class MockGpioDriver : GpioDriver
     {
+        private Dictionary<int, PinValue> _pinStates = new Dictionary<int, PinValue>();
+
         protected override int PinCount => 40;
 
         protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
@@ -45,6 +48,7 @@ namespace RemoteRelay
         protected override void Write(int pinNumber, PinValue value)
         {
             Console.WriteLine($"Writing value {value} to pin {pinNumber}");
+            _pinStates[pinNumber] = value;
         }
 
         protected override WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
@@ -56,7 +60,7 @@ namespace RemoteRelay
         protected override PinValue Read(int pinNumber)
         {
             Console.WriteLine($"Reading value from pin {pinNumber}");
-            return PinValue.Low;
+            return _pinStates.ContainsKey(pinNumber) ? _pinStates[pinNumber] : PinValue.Low;
         }
 
         protected override void AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback)
